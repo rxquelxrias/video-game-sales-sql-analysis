2de387b36378f3c1b76bbdb202e71ce3a2500400
Raquel Arias Sancho <137843959+rxquelxrias@users.noreply.github.com>
2026-03-12 09:01:56 -0600

    Add files via upload

---
+++ database/staging_table.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE staging_vgsales (
+	rank INT,
+	name TEXT,
+	platform TEXT,
+	year TEXT,
+	genre TEXT,
+	publisher TEXT,
+	na_sales NUMERIC,
+	eu_sales NUMERIC,
+	jp_sales NUMERIC,
+	other_sales NUMERIC,
+	global_sales NUMERIC
+);
+
